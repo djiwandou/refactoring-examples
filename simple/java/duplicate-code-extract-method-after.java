ed91db60788e6bfc10b9b418deb85aae94c12c8f
Djiwandou Putro <djiwandou@tokodistributor.com>
2019-10-10 14:29:00 +0700

    add duplicate code, solved with extract method in java

---
+++ simple/java/duplicate-code-extract-method-after.java
@@ -0,0 +1,21 @@
+public class MedicalRecord
+{
+    public DateTime DateArchived { get; private set; }
+    public bool Archived { get; private set; }
+    
+    public void ArchiveRecord()
+    {
+        SwitchToArchived();
+    }
+
+    public void CloseRecord()
+    {
+        SwitchToArchived();
+    }
+
+    private void SwitchToArchived()
+    {
+        Archived = true;
+        DateArchived = DateTime.Now;
+    }
+}
---
+++ simple/java/duplicate-code-extract-method-before.java
@@ -0,0 +1,17 @@
+public class MedicalRecord
+{
+    public DateTime DateArchived { get; private set; }
+    public bool Archived { get; private set; }
+
+    public void ArchiveRecord()
+    {
+        Archived = true;
+        DateArchived = DateTime.Now;
+   }
+
+   public void CloseRecord()
+   {
+       Archived = true;
+       DateArchived = DateTime.Now;
+   }
+}
